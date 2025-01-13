77d03db6bffeebe81325ef3626b5730542b7a205
Hannah Osei <hannahosei97@gmail.com>
2025-01-13 10:17:32 +0000

    added validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
 
-  validates :comment, presence: true
+  validates :recipe, presence: true
+  validates :category, presence: true
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :recipe_id, uniqueness: { scope: :category_id, message: "has already been bookmarked in this category" }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,7 @@ class Recipe < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :categories, through: :bookmarks
 
-  validates :name, :description, :image_url, :rating, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
